0fe0695466807593ed54c3d13b1fce8f2d122731
Sevag Hanssian <shanssian@nvidia.com>
2022-09-22 11:49:43 -0400

    Replace && with ; for set -e to work

--- docker/build_scripts/install-rapids-tools.sh
+++ docker/build_scripts/install-rapids-tools.sh
@@ -6,7 +6,7 @@ set -Eexuo pipefail
 if which yum; then
   yum install -y wget
 else
-  apt update && apt install -y --no-install-recommends \
+  apt update ; apt install -y --no-install-recommends \
       curl \
       wget \
       numactl \
@@ -25,16 +25,16 @@ fi
 
 export SCCACHE_VERSION=0.2.15
 curl -o /tmp/sccache.tar.gz \
-        -L "https://github.com/mozilla/sccache/releases/download/v${SCCACHE_VERSION}/sccache-v${SCCACHE_VERSION}-${AUDITWHEEL_ARCH}-unknown-linux-musl.tar.gz" &&\
-        tar -C /tmp -xvf /tmp/sccache.tar.gz &&\
-        mv "/tmp/sccache-v${SCCACHE_VERSION}-${AUDITWHEEL_ARCH}-unknown-linux-musl/sccache" /usr/bin/sccache &&\
+        -L "https://github.com/mozilla/sccache/releases/download/v${SCCACHE_VERSION}/sccache-v${SCCACHE_VERSION}-${AUDITWHEEL_ARCH}-unknown-linux-musl.tar.gz" ;\
+        tar -C /tmp -xvf /tmp/sccache.tar.gz ;\
+        mv "/tmp/sccache-v${SCCACHE_VERSION}-${AUDITWHEEL_ARCH}-unknown-linux-musl/sccache" /usr/bin/sccache ;\
         chmod +x /usr/bin/sccache
 
 export UCX_VERSION=1.13.0
-mkdir -p /ucx-src /usr && cd /ucx-src \
- && git clone https://github.com/openucx/ucx -b v${UCX_VERSION} ucx-git-repo && cd ucx-git-repo \
- && ./autogen.sh \
- && ./contrib/configure-release \
+mkdir -p /ucx-src /usr ; cd /ucx-src \
+ ; git clone https://github.com/openucx/ucx -b v${UCX_VERSION} ucx-git-repo ; cd ucx-git-repo \
+ ; ./autogen.sh \
+ ; ./contrib/configure-release \
     --prefix=/usr               \
     --enable-mt                 \
     --enable-cma                \
@@ -45,10 +45,10 @@ mkdir -p /ucx-src /usr && cd /ucx-src \
     --with-sysroot              \
     --with-cuda=/usr/local/cuda \
     CPPFLAGS=-I/usr/local/cuda/include \
- && make -j \
- && make install \
- && cd /usr \
- && rm -rf /ucx-src/
+ ; make -j \
+ ; make install \
+ ; cd /usr \
+ ; rm -rf /ucx-src/
 
 # Install latest gha-tools
 wget https://github.com/rapidsai/gha-tools/releases/latest/download/tools.tar.gz -O - | tar -xz -C /usr/local/bin
